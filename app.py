11ecd1a2cb9652d549a41b553806dd9c52e13be4
Ssali Alex <kartsali.alex@gmail.com>
2018-05-31 19:11:30 +0300

    removed commented out code in app.py

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ details=[{'id':'1','name':'Alex','dop':'17/06/18','top':'13:00','item requested 
         {'id':'2','name':'Peter','dop':'18/06/18','top':'14:00','item requested for':'brakes'},
         {'id':'3','name':'Denis','dop':'17/06/18','top':'13:40','item requested for':'engine'}]
 
-# def get_request_details(name):
-#     return [details for details in details if details["name"] == name]
-# def _get_details(id):
-#     return [details for details in details if details['id'] == id]
-# def length():
-#     return len(details)
+
 
 @app.route('/api/v1/request/', methods=['GET'])
 def _get_details():
@@ -20,16 +15,7 @@ def _get_details():
 def create_item():
     detail = request.get_json()  
     details.append(detail)
-    return jsonify(message = 'Request added'), 200
-
-# @app.route('/api/v1/request/<int:id>', methods=['PUT'])
-# def update_item(id):
-#     name = request.json.get('name', details[0]['name'])
-#     value = request.json.get('value', details[0]['value'])  
-#     details[0]['name'] = name
-#     details[0]['value'] = value
-#     return jsonify({'item': details[0]}), 200
- 
+    return jsonify(message = 'Request added'), 200 
 
     
 
